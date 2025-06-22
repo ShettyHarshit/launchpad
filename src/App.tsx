757ad81b599eff0419b3444160bebd399c8f1318
Harshit Shetty <swaharsh@gmail.com>
2025-06-22 18:35:38 +0530

    feat: initial commit with Vite Setup + temporary page

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="homepage">
+      <h1 className="homepage__title">👋 Yo, I’m Harshit.</h1>
+      <p className="homepage__subtitle">
+        This server is live.
+        <br />
+        More things coming soon.
+      </p>
+    </main>
+  );
+}
+
+export default App;
